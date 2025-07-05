80253f8e9a9cdcbe89d210f906733024bd6c1763
Bennet van der Linden <Bennet.vanderLinden@wearetriple.com>
2025-07-05 12:09:55 +0200

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Bennet van der Linden on 05/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
