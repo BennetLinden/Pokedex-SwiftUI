f87504a8348195f47a9318198d769671a11eafbf
Bennet van der Linden <vanderlinden.bennet@gmail.com>
2025-07-15 15:12:32 +0200

    Add ScrollView to PokemonDetailsView

--- Pokedex/Presentation/Features/PokemonDetailsView.swift
+++ Pokedex/Presentation/Features/PokemonDetailsView.swift
@@ -15,35 +15,38 @@ struct PokemonDetailsView: View {
     @State private var pokemon: Pokemon?
     
     var body: some View {
-        VStack {
-            PokemonHeaderView(
-                name: pokemonReference.name,
-                id: pokemonReference.id,
-                types: pokemon?.types.map(\.type) ?? []
-            )
-            
-            AsyncImage(url: pokemon?.imageURL) { phase in
-                switch phase {
-                case .empty:
-                    ProgressView()
-                case .success(let image):
-                    image
-                        .resizable()
-                        .scaledToFit()
-                case .failure:
-                    EmptyView()
-                @unknown default:
-                    EmptyView()
+        ScrollView {
+            VStack {
+                PokemonHeaderView(
+                    name: pokemonReference.name,
+                    id: pokemonReference.id,
+                    types: pokemon?.types.map(\.type) ?? []
+                )
+                
+                AsyncImage(url: pokemon?.imageURL) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    case .failure:
+                        EmptyView()
+                    @unknown default:
+                        EmptyView()
+                    }
+                }
+                .frame(maxHeight: .infinity, alignment: .top)
+                .aspectRatio(1.0, contentMode: .fit)
+                
+                if let pokemon {
+                    PokemonAboutView(pokemon: pokemon)
+                        .padding(.bottom, 24)
                 }
             }
-            .frame(maxHeight: .infinity, alignment: .top)
-            
-            if let pokemon {
-                PokemonAboutView(pokemon: pokemon)
-                    .padding(.bottom, 24)
-            }
+            .padding()
         }
-        .padding()
         .task {
             do {
                 pokemon = try await getPokemonDetails(
